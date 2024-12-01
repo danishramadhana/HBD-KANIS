fcb2149d02da751d856dabc1a6604c1a81e6aeae
danishramadhana <dhana.rama12369@gmail.com>
2024-12-01 23:59:59 +0700

    upgrt

--- sketch.js
+++ sketch.js
@@ -12,79 +12,92 @@ var total = 12;
 var font;
 var foto, bgStatic;
 var txt = "HAPPY BIRTHDAY";
-var name = "AINUNNISA MALLAPiSENG";
+var name = "AINUNNISA MALLAPISENG";
 var sfxPop;
+var maskImage;
+var imgWidth = 200;
+var imgHeight = 200;
 
 function preload() {
-	foto = loadImage("assets/img/FOTO.png");
-	bgStatic = loadImage("https://png.pngtree.com/thumb_back/fw800/background/20221028/pngtree-abstract-pink-background-image_1469866.jpg");
-	font = loadFont("assets/font/FredokaOne.ttf");
-	sfxPop = loadSound("assets/sound/assets_sfx_pop.mp3");
+    foto = loadImage("assets/img/FOTO.png");
+    bgStatic = loadImage("https://png.pngtree.com/thumb_back/fw800/background/20221028/pngtree-abstract-pink-background-image_1469866.jpg");
+    font = loadFont("assets/font/FredokaOne.ttf");
+    sfxPop = loadSound("assets/sound/assets_sfx_pop.mp3");
+
+    // Create a circular mask
+    maskImage = createGraphics(imgWidth, imgHeight);
+    maskImage.ellipse(maskImage.width / 2, maskImage.height / 2, imgWidth, imgHeight);
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
 
-	for (let i = 0; i < total; i++) {
-		balloons.push(new Ballon());
-	}
+    for (let i = 0; i < total; i++) {
+        balloons.push(new Ballon());
+    }
+
+    // Apply mask to the photo
+    foto.mask(maskImage);
 }
 
 function draw() {
-	background(114, 51, 153);
-	
-	image(bgStatic, 0, 0, width, height);
+    background(114, 51, 153);
+    
+    image(bgStatic, 0, 0, width, height);
 
-	push();
-	var imgX = width / 2;
-	var imgY = height / 2;
-	// foto
-	translate(imgX + (-mouseX + width / 2) / 30, imgY + (-mouseY + height / 2) / 30);
-	imageMode(CENTER);
-	image(foto, 0, 0, 150, 200);
-	// frame
-	noFill();
-	stroke(200, 120);
-	strokeWeight(6);
-	ellipse(0, 0, 210, 210);
-	pop();
+    push();
+    var imgX = width / 2;
+    var imgY = height / 2;
+    translate(imgX + (-mouseX + width / 2) / 30, imgY + (-mouseY + height / 2) / 30);
+    imageMode(CENTER);
+    
+    // Image inside the circle with original aspect ratio
+    image(foto, 0, 0, imgWidth, imgHeight);
+    pop();
 
-	// name
-	push();
-	fill(255);
-	stroke(45, 28, 82);
-	strokeWeight(7);
-	textFont(font);
-	textSize(29);
-	textAlign(CENTER);
-	// name
-	text(name, imgX + (mouseX - width / 2) / 30, imgY + 200 + (mouseY - height / 2) / 30);
-	// teks hbd
-	text(txt, imgX + (mouseX - width / 2) / 30, imgY - 150 + (mouseY - height / 2) / 30);
-	pop();
+    // Frame moving with photo
+    push();
+    noFill();
+    stroke(200, 120);
+    strokeWeight(6);
+    translate(imgX + (-mouseX + width / 2) / 30, imgY + (-mouseY + height / 2) / 30);
+    ellipse(0, 0, imgWidth, imgHeight);
+    pop();
 
-	// balloons
-	for (let i = 0; i < balloons.length; i++) {
-		balloons[i].show();
-		balloons[i].up();
-		balloons[i].checkEdge()
-		if (balloons[i].mouseHover()) {
-			sfxPop.play();
-			balloons.splice(i, 1);
-		}
-	}
+    // Name and text
+    push();
+    fill(255);
+    stroke(45, 28, 82);
+    strokeWeight(7);
+    textFont(font);
+    textSize(29);
+    textAlign(CENTER);
+    text(name, imgX + (mouseX - width / 2) / 30, imgY + 200 + (mouseY - height / 2) / 30);
+    text(txt, imgX + (mouseX - width / 2) / 30, imgY - 150 + (mouseY - height / 2) / 30);
+    pop();
 
-	if (balloons.length < 3) {
-		for (let i = 0; i < total; i++) {
-			balloons.push(new Ballon());
-		}
-	}
+    // Balloons
+    for (let i = 0; i < balloons.length; i++) {
+        balloons[i].show();
+        balloons[i].up();
+        balloons[i].checkEdge()
+        if (balloons[i].mouseHover()) {
+            sfxPop.play();
+            balloons.splice(i, 1);
+        }
+    }
+
+    if (balloons.length < 3) {
+        for (let i = 0; i < total; i++) {
+            balloons.push(new Ballon());
+        }
+    }
 }
 
 function keyPressed() {
-	if (key === 'z' || key === 'Z') {
-		for (let i = 0; i < total; i++) {
-			balloons.push(new Ballon());
-		}
-	}
+    if (key === 'z' || key === 'Z') {
+        for (let i = 0; i < total; i++) {
+            balloons.push(new Ballon());
+        }
+    }
 }
